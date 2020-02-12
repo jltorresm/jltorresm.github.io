aa2e629477bff7d19dd1c004a09c58f1c7256343
Jose Torres <jose.torres.tm@gmail.com>
2020-02-12 09:18:18 -0500

    Add the wellness timers.

---
+++ _pages/wellness.md
@@ -0,0 +1,65 @@
+---
+layout: single
+title: Focus and Wellness Timers
+permalink: /wellness-timers
+---
+
+<div id="wellness-timers-container">
+	<div class="timer pomodoro">
+		<h1>Pomodoro Timer</h1>
+		<p class="explanation">For a quick explanation about the Pomodoro Technique,
+			go <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique">here</a>.
+		</p>
+		<div class="timer-container">
+			<div class="timer-row">
+				<label>Pomodoro</label>
+				<div id="pomodoro" class="timer-body">25:00</div>
+			</div>
+			<div class="timer-row">
+				<label>Short Break</label>
+				<div id="short" class="timer-body">05:00</div>
+			</div>
+			<div class="timer-row">
+				<label>Long Break</label>
+				<div id="long" class="timer-body">15:00</div>
+			</div>
+		</div>
+		<button class="start btn btn--info btn--large"><i class="fas fa-stopwatch"></i><span>start</span></button>
+		<button class="reset btn btn--inverse btn--large"><i class="fas fa-trash"></i>reset current</button>
+	</div>
+	<div class="timer strain">
+		<h1>Screen strain timer</h1>
+		<p class="explanation">This timer tries to help to reduce the eye strain
+			caused by working long hours in front of a screen. It also introduces a
+			secondary timer with instructions on how reduce body strain for working
+			in an office environment.</p>
+		<p class="explanation">A simple way to prevent <b>eye strain</b> is the
+			20-20-20 rule: every 20 minutes, focus the eyes on an object 20 feet
+			(6 meters) away for 20 seconds.</p>
+		<div class="timer-container">
+			<div class="timer-row eye-strain">
+				<label>Eye Strain</label>
+				<div class="timer-body">20:00</div>
+			</div>
+		</div>
+		<button class="start btn btn--info btn--large"><i class="fas fa-stopwatch"></i><span>start</span></button>
+		<button class="reset btn btn--inverse btn--large"><i class="fas fa-trash"></i>reset</button>
+	</div>
+</div>
+
+## Further Reading and References
+
+**The Pomodoro Technique**. A very simple, low tech time management technique.<br>
+Taken from Wikipedia: [Pomodoro Technique](https://en.wikipedia.org/wiki/Pomodoro_Technique).<br>
+Taken from the creator's website: [Do more and have fun with time management](https://francescocirillo.com/pages/pomodoro-technique).
+{: .notice--warning}
+
+**Eye and Body Strain**. When working a 9 to 5 job in front of a computer we usually don't think about the consequences it has for our body.<br>
+Taken from Wikipedia: [Computer Vision Syndrome](https://en.wikipedia.org/wiki/Computer_vision_syndrome#Therapy).<br>
+Taken from the Canadian Association of Optometrists: [What is the 20-20-20 rule?](https://www.youtube.com/watch?v=2GxXIftBUA0).<br>
+Taken from Visian ICL: [The 20-20-20 Rule: Preventing Digital Eye Strain](https://us.discovericl.com/blog/the-20-20-20-rule-preventing-digital-eye-strain).<br>
+Taken from the Victoria State Government's Better Health Channel: [Computer Related Injuries](https://www.betterhealth.vic.gov.au/health/healthyliving/computer-related-injuries).<br>
+Taken from the University Of Michigan's University Health Service: [Computer Ergonomics: How to Protect Yourself from Strain and Pain](https://www.uhs.umich.edu/computerergonomics).
+{: .notice--warning}
+
+<script src="/assets/js/wellness.js"></script>
---
+++ assets/js/wellness.js
@@ -0,0 +1,323 @@
+/*******************************************************************************
+ * START :: Commons
+ ******************************************************************************/
+//
+// Timer "Class"
+//
+function Timer(initialValue, onRender, onFinish) {
+	//
+	// Timer properties :: Logic
+	//
+	this.initialValue = initialValue;
+	this.currentValue = initialValue;
+	this.interval = null;
+	this.running = false;
+
+	//
+	// Timer properties :: DOM
+	//
+	this.renderCallback = onRender;
+	this.finishCallback = onFinish;
+}
+
+//
+// Starts the timer
+//
+Timer.prototype.start = function () {
+	const me = this;
+	me.running = true;
+
+	// First adjust any discrepancy with the ui
+	me.render();
+
+	me.interval = setInterval(function () {
+		me.currentValue--;
+		me.render();
+		if (me.currentValue === 0) {
+			me.finish();
+		}
+	}, 1000);
+};
+
+//
+// Stops the timer
+//
+Timer.prototype.stop = function () {
+	this.running = false;
+	clearInterval(this.interval);
+	this.render();
+};
+
+//
+// Starts or stops the timer
+//
+Timer.prototype.startStop = function () {
+	const isRunning = this.isRunning();
+	if (isRunning) {
+		this.stop();
+	} else {
+		this.start();
+	}
+
+	return !isRunning;
+};
+
+//
+// Returns true if the timer is running
+//
+Timer.prototype.isRunning = function () {
+	return this.running;
+};
+
+//
+// Resets the timer
+//
+Timer.prototype.reset = function () {
+	this.stop();
+	this.currentValue = this.initialValue;
+	this.render();
+};
+
+//
+// Calls the render callback
+//
+Timer.prototype.render = function () {
+	this.renderCallback(this.currentValue, this.running);
+};
+
+//
+// Calls the finish callback
+//
+Timer.prototype.finish = function () {
+	this.reset();
+	this.finishCallback(this.initialValue);
+};
+
+//
+// Format string as mm:ss clock format
+//
+String.prototype.toMMSS = function () {
+	let total = parseInt(this, 10);
+	let minutes = Math.floor(total / 60);
+	let seconds = total - (minutes * 60);
+
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+
+	return minutes + ':' + seconds;
+};
+
+// TODO: Merge pomodoro and timerswitcher classes into a generic periodic timer class.
+
+//
+// Pomodoro "Class"
+//
+function Pomodoro(pomodoro, short, long, onRender, onSwitch) {
+	this.pomodoroDuration = pomodoro;
+	this.short = short;
+	this.long = long;
+	this.cycle = 8;
+	this.iteration = 0;
+
+	this.timer = new Timer(this.pomodoroDuration, this.manageRender.bind(this), this.switchPeriod.bind(this));
+
+	this.renderCallback = onRender;
+	this.switchCallback = onSwitch;
+}
+
+Pomodoro.prototype.startStop = function () {
+	if (this.timer.isRunning()) {
+		this.timer.stop();
+	} else {
+		this.timer.start();
+	}
+};
+
+Pomodoro.prototype.reset = function () {
+	this.timer.reset();
+};
+
+Pomodoro.prototype.manageRender = function (currentValue, isRunning) {
+	this.renderCallback(this.getStage(), currentValue, isRunning);
+};
+
+Pomodoro.prototype.switchPeriod = function () {
+	this.switchCallback();
+
+	this.iteration++;
+
+	if (this.getStage() === "s")
+		this.timer = new Timer(this.short, this.manageRender.bind(this), this.switchPeriod.bind(this));
+	else if (this.getStage() === "l")
+		this.timer = new Timer(this.long, this.manageRender.bind(this), this.switchPeriod.bind(this));
+	else
+		this.timer = new Timer(this.pomodoroDuration, this.manageRender.bind(this), this.switchPeriod.bind(this));
+
+	this.startStop();
+};
+
+Pomodoro.prototype.getStage = function () {
+	const map = "pspspspl";
+	let i = this.iteration;
+	return map[i % this.cycle];
+};
+
+//
+// Switches between 2 different intervals
+//
+function TimerSwitcher(main, alt, onRender, onSwitch) {
+	this.mainDuration = main;
+	this.altDuration = alt;
+
+	this.cycle = 2;
+	this.iteration = 0;
+
+	this.timer = new Timer(this.mainDuration, this.manageRender.bind(this), this.switchPeriod.bind(this));
+
+	this.renderCallback = onRender;
+	this.switchCallback = onSwitch;
+}
+
+TimerSwitcher.prototype.startStop = function () {
+	if (this.timer.isRunning()) {
+		this.timer.stop();
+	} else {
+		this.timer.start();
+	}
+};
+
+TimerSwitcher.prototype.reset = function () {
+	this.timer.reset();
+};
+
+TimerSwitcher.prototype.manageRender = function (currentValue, isRunning) {
+	this.renderCallback(this.getStage(), currentValue, isRunning);
+};
+
+TimerSwitcher.prototype.switchPeriod = function () {
+	this.switchCallback();
+
+	this.iteration++;
+
+	if (this.getStage() === "a")
+		this.timer = new Timer(this.altDuration, this.manageRender.bind(this), this.switchPeriod.bind(this));
+	else
+		this.timer = new Timer(this.mainDuration, this.manageRender.bind(this), this.switchPeriod.bind(this));
+
+	this.startStop();
+};
+
+TimerSwitcher.prototype.getStage = function () {
+	const map = "ma";
+	let i = this.iteration;
+	return map[i % this.cycle];
+};
+
+//
+// Rings a bell
+//
+function ringBell() {
+	const audio = new Audio('/assets/audio/bell.mp3');
+	audio.play();
+}
+
+/*******************************************************************************
+ * END :: Commons
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * START :: Pomodoro setup
+ ******************************************************************************/
+const $etaPom = document.querySelector(".timer.pomodoro #pomodoro");
+const $etaShort = document.querySelector(".timer.pomodoro #short");
+const $etaLong = document.querySelector(".timer.pomodoro #long");
+
+const $startPom = document.querySelector(".timer.pomodoro button.start");
+const $resetPom = document.querySelector(".timer.pomodoro button.reset");
+
+let pomodoro = new Pomodoro(25 * 60, 5 * 60, 15 * 60, renderPomodoro, ringBell);
+
+$startPom.onclick = function () {
+	pomodoro.startStop();
+};
+
+$resetPom.onclick = function () {
+	pomodoro.reset();
+};
+
+function renderPomodoro(stage, currentValue, isRunning) {
+	let $ele = $etaPom;
+	if (stage === "s")
+		$ele = $etaShort;
+	else if (stage === "l")
+		$ele = $etaLong;
+
+	$ele.innerHTML = currentValue.toString().toMMSS();
+
+	if (isRunning) {
+		$ele.classList.add("active");
+		$startPom.classList.add("active");
+		$startPom.querySelector("span").innerHTML = "stop";
+
+	} else {
+		$ele.classList.remove("active");
+		$startPom.classList.remove("active");
+		$startPom.querySelector("span").innerHTML = "start";
+	}
+}
+
+/*******************************************************************************
+ * END :: Pomodoro setup
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * START :: Strain setup
+ ******************************************************************************/
+const $startStrain = document.querySelector(".timer.strain button.start");
+const $resetStrain = document.querySelector(".timer.strain button.reset");
+const $eyeStrain = document.querySelector(".timer.strain .timer-row.eye-strain");
+
+const $eyeETA = $eyeStrain.querySelector(".timer-body");
+const $eyeLabel = $eyeStrain.querySelector("label");
+
+let strainTimer = new TimerSwitcher(20 * 60, 20, renderTimer, ringBell);
+
+$startStrain.onclick = function () {
+	strainTimer.startStop();
+};
+
+$resetStrain.onclick = function () {
+	strainTimer.reset();
+};
+
+function renderTimer(stage, currentValue, isRunning) {
+	if (stage === "m") {
+		$eyeLabel.innerHTML = "Eye Strain";
+		$eyeStrain.classList.remove("rest");
+	} else if (stage === "a") {
+		$eyeLabel.innerHTML = "Rest";
+		$eyeStrain.classList.add("rest");
+	}
+
+	$eyeETA.innerHTML = currentValue.toString().toMMSS();
+
+	if (isRunning) {
+		$startStrain.classList.add("active");
+		$startStrain.querySelector("span").innerHTML = "stop";
+
+	} else {
+		$startStrain.classList.remove("active");
+		$startStrain.querySelector("span").innerHTML = "start";
+	}
+}
+
+/*******************************************************************************
+ * END :: String setup
+ ******************************************************************************/
