5bba8e68c2f741c10a1702e1a92e6fe08347d6c8
Jose Torres <jose.torres.tm@gmail.com>
2020-08-07 09:32:31 -0500

    Add the calculator page including the initial times calculator.

---
+++ _pages/calculators.md
@@ -0,0 +1,62 @@
+---
+layout: single
+title: Some Random Calculators
+permalink: /calculators
+---
+
+<div id="time-calculator">
+    <header class="header">
+        <h1>Times</h1>
+        <p class="explanation">Add times in the format <em>hh:mm:ss</em> 
+        or an ISO 8601 duration, e.g.:<em>P1Y2M3DT4H5M6S</em>.
+        <br>Double click on a time to edit it.
+        <br>Click on the <em>clear</em> button to remove all current times.</p>
+        <div class="form">
+            <input
+                class="new-time"
+                autocomplete="off"
+                placeholder="Add time..."
+                v-model="newTime"
+                v-on:keyup.enter="addTime"
+            />
+            <button class="btn btn--info btn--large" v-on:click="clearTimes">
+                <i class="fas fa-trash"></i><span>clear</span>
+                </button>
+        </div>
+    </header>
+    <section class="main" v-show="times.length" v-cloak>
+        <ul class="time-list">
+            <li
+                v-for="time in times"
+                class="time"
+                v-bind:key="time.id"
+                v-bind:class="{ editing: time == editedTime }"
+            >
+                <div class="view">
+                    <label v-on:dblclick="editTime(time)">[[ time.value ]]</label>
+                    <button class="destroy" v-on:click="removeTime(time)"></button>
+                </div>
+                <input
+                    class="edit"
+                    type="text"
+                    v-model="time.value"
+                    v-time-focus="time == editedTime"
+                    v-on:blur="doneEdit(time)"
+                    v-on:keyup.enter="doneEdit(time)"
+                    v-on:keyup.esc="cancelEdit(time)"
+                />
+            </li>
+        </ul>
+    </section>
+    <section class="results">
+        <label v-on:click="changeTotalTimeRender()">Sum:<em>[[ totalTimeFormatted ]]</em></label>
+        <label v-on:click="changeAverageTimeRender()">Average:<em>[[ averageTimeFormatted ]]</em></label>
+    </section>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+<script src="/assets/js/dist/moment-duration-format.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+# <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script src="/assets/js/calculators.js"></script>
+    
---
+++ assets/js/calculators.js
@@ -0,0 +1,150 @@
+Vue.config.devtools = true;
+
+function asMilliseconds(value) {
+	const duration = moment.duration(value);
+
+	if (!duration.isValid()) {
+		return 0;
+	}
+
+	return duration.as("milliseconds");
+}
+
+function formatAsSeconds(milliseconds) {
+	const d = moment.duration(milliseconds, "milliseconds");
+	return d.asSeconds() + " seconds";
+}
+
+function formatAsClock(milliseconds) {
+	const d = moment.duration(milliseconds, "milliseconds");
+	return d.format("hh[h]mm[m]ss[s]");
+}
+
+function formatAsISO(milliseconds) {
+	const d = moment.duration(milliseconds, "milliseconds");
+	return d.toISOString();
+}
+
+// Time Calculator Vue instance
+new Vue({
+	el: "#time-calculator",
+
+	// Change delimiters to avoid conflicts
+	delimiters: ['[[', ']]'],
+
+	// app initial state
+	data: {
+		uid: 1,
+		times: [
+			{id: 0, value: "PT5M30S", milliseconds: 330000},
+			{id: 1, value: "00:03:30", milliseconds: 210000}
+		],
+		newTime: "",
+		editedTime: null,
+
+		renderTypes: [formatAsSeconds, formatAsClock, formatAsISO],
+		totalTimeRenderer: 0,
+		averageTimeRenderer: 0,
+	},
+
+	// computed properties
+	// http://vuejs.org/guide/computed.html
+	computed: {
+		totalTime: function () {
+			return this.times.reduce((acc, time) => acc + time.milliseconds, 0);
+		},
+
+		averageTime: function () {
+			if (this.times.length === 0) {
+				return 0;
+			}
+			return this.totalTime / this.times.length;
+		},
+
+		totalTimeFormatted: function () {
+			return this.renderTypes[this.totalTimeRenderer](this.totalTime);
+		},
+
+		averageTimeFormatted: function () {
+			return this.renderTypes[this.averageTimeRenderer](this.averageTime);
+		}
+	},
+
+	filters: {},
+
+	// methods that implement data logic.
+	// note there's no DOM manipulation here at all.
+	methods: {
+		clearTimes: function () {
+			this.times = [];
+		},
+
+		addTime: function () {
+			const value = this.newTime && this.newTime.trim();
+			if (!value) {
+				return;
+			}
+
+			const milliseconds = asMilliseconds(value);
+			if (milliseconds === 0) {
+				return;
+			}
+
+			this.times.push({id: this.uid++, value: value, milliseconds: milliseconds});
+			this.newTime = "";
+		},
+
+		editTime: function (time) {
+			this.beforeEditCache = time.milliseconds;
+			this.editedTime = time;
+		},
+
+		removeTime: function (time) {
+			this.times.splice(this.times.indexOf(time), 1);
+		},
+
+		doneEdit: function (time) {
+			if (!this.editedTime) {
+				return;
+			}
+			const trimmedTime = time.value.trim();
+			if (!trimmedTime) {
+				this.removeTime(time);
+				return;
+			}
+
+			const milliseconds = asMilliseconds(trimmedTime);
+			if (milliseconds === 0) {
+				return;
+			}
+
+			time.value = trimmedTime;
+			time.milliseconds = milliseconds;
+			this.editedTime = null;
+		},
+
+		cancelEdit: function (time) {
+			this.editedTime = null;
+			time.value = this.beforeEditCache;
+		},
+
+		changeTotalTimeRender: function () {
+			this.totalTimeRenderer = ++this.totalTimeRenderer % this.renderTypes.length;
+		},
+
+		changeAverageTimeRender: function () {
+			this.averageTimeRenderer = ++this.averageTimeRenderer % this.renderTypes.length;
+		}
+	},
+
+	// a custom directive to wait for the DOM to be updated
+	// before focusing on the input field.
+	// http://vuejs.org/guide/custom-directive.html
+	directives: {
+		"time-focus": function (el, binding) {
+			if (binding.value) {
+				el.focus();
+			}
+		}
+	}
+});
